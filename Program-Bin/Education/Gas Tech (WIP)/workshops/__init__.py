07ba8c54e1d320cabdfe790b0adc8ec0fb3e8841
Matthew Pidlysny <mattpidlysny@gmail.com>
2025-12-22 04:42:16 -0500

    Delete Program-Bin/Education/Gas Tech (WIP) directory

--- Program-Bin/Education/Gas Tech (WIP)/workshops/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Gas Tech Workshops Package
-Professional-grade calculation and design workshops
-"""
-
-from .heat_load_calculator import HeatLoadCalculator
-from .piping_system_designer import PipingSystemDesigner
-
-__all__ = ['HeatLoadCalculator', 'PipingSystemDesigner']
